c82cf7702776532563b0ddcea77e89658bcb58cf
旧亚楠\Gascoigne <415753928@qq.com>
2020-12-18 14:45:34 +0800

    游戏界面搭建1

--- assets/script/EnterGame.js
+++ assets/script/EnterGame.js
@@ -3,12 +3,15 @@ cc.Class({
 
     properties:
     {
-
+        GamePlay: cc.Node,
+        Main:cc.Node
     },
 
     onLoad()
     {
         cc.log("onLoad");
+        this.Main.active = true;
+        this.GamePlay.active = false;
     },
 
     start()
@@ -18,7 +21,14 @@ cc.Class({
 
     clickBtn(sender,num)
     {
-        cc.log("click button:"+num);
+        cc.log("click button:" + num);
+        this.Main.active = false;
+        this.GamePlay.active = true;
+        if (num == "return")
+        {
+            this.Main.active = true;
+            this.GamePlay.active = false;
+        }
     }
     // update (dt) {},
 });
