9e8f7932a23dcd8b5cf916e8453d22ad341873a7
旧亚楠\Gascoigne <415753928@qq.com>
2020-12-18 15:34:27 +0800

    生成块

--- assets/script/EnterGame.js
+++ assets/script/EnterGame.js
@@ -6,6 +6,7 @@ cc.Class({
         GamePlay: cc.Node,
         Main: cc.Node,
         Prefab_item: cc.Prefab,
+        itemParent:cc.Node,
     },
 
     onLoad()
@@ -13,7 +14,7 @@ cc.Class({
         cc.log("onLoad");
         this.Main.active = true;
         this.GamePlay.active = false;
-        this.addItem();
+        this.init();
     },
 
     start()
@@ -26,6 +27,7 @@ cc.Class({
         cc.log("click button:" + num);
         this.Main.active = false;
         this.GamePlay.active = true;
+        this.numItem = num;//行列
         if (num == "return")
         {
             this.Main.active = true;
@@ -33,20 +35,34 @@ cc.Class({
         }
     },
 
-    addItem()
+    addItem:function()
     {
-        for (var i = 0; i < 3; i++)
+        var posStart = cc.v2(-this.itemParentW / 2 + this.interval, -this.itemParentW / 2 + this.interval);
+        for (var i = 0; i < this.numItem; i++)
         {
-            for (var j = 0; j < 3; j++)
+            for (var j = 0; j < this.numItem; j++)
             {
                 var node = cc.instantiate(this.Prefab_item);
-                node.parent = this.GamePlay;
-                node.width = 100;
-                node.height = 100;
-                node.x = (node.width + 5) * i;
-                node.y = (node.height + 5) * j;
+                node.parent = this.itemParent;
+                node.width = this.itemW;
+                node.height = this.itemW;
+                node.anchorX = 0;
+                node.anchorY = 0;
+                node.x = posStart.x+(node.width + 5) * i;
+                node.y = posStart.y+(node.height + 5) * j;
             }
         }
+    },
+
+    init: function ()
+    {
+       
+        this.interval = 5;//间隔
+        this.itemW = Math.round(650 / this.numItem);//块大小
+        this.itemParentW = this.numItem * this.itemW + (this.numItem + 1) * this.interval;//背景大小    
+        this.itemParent.width = this.itemParentW;
+        this.itemParent.height = this.itemParentW;
+        this.addItem();
     }
     // update (dt) {},
 });
