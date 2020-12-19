3c61303abe5db88f4f43762522aefbcfab60fe81
旧亚楠\Gascoigne <415753928@qq.com>
2020-12-19 20:10:42 +0800

    从数组初始化游戏数据

--- assets/script/EnterGame.js
+++ assets/script/EnterGame.js
@@ -5,33 +5,29 @@ cc.Class({
     {
         GamePlay: cc.Node,
         Main: cc.Node,
-        Prefab_item: cc.Prefab,
-        itemParent:cc.Node,
+        pre_item_bg: cc.Prefab,
+        pre_item: cc.Prefab,
+        itemParent: cc.Node,
     },
 
-    onLoad()
-    {
+    onLoad() {
         cc.log("onLoad");
         this.Main.active = true;
         this.GamePlay.active = false;
     },
 
-    start()
-    {
+    start() {
         cc.log("onStart");
     },
 
-    clickBtn(sender,num)
-    {
+    clickBtn(sender, num) {
         cc.log("click button:" + num);
-        if (num == "return")
-        {
+        if (num == "return") {
             this.Main.active = true;
             this.GamePlay.active = false;
-            this.   cleanItem();
+            this.cleanItem();
         }
-        else
-        {
+        else {
             this.Main.active = false;
             this.GamePlay.active = true;
             this.numItem = parseInt(num);//行列
@@ -39,38 +35,67 @@ cc.Class({
         }
     },
 
-    addItem:function()
-    {
-        var posStart = cc.v2(-this.itemParentW / 2 + this.interval, -this.itemParentW / 2 + this.interval);
-        for (var i = 0; i < this.numItem; i++)
-        {
-            for (var j = 0; j < this.numItem; j++)
-            {
-                var node = cc.instantiate(this.Prefab_item);
+    addItemBg: function () {
+        var posStart = cc.v2(-this.itemParentW / 2 + this.itemW / 2 + this.interval, -this.itemParentW / 2 + this.itemW / 2 + this.interval);
+        for (var i = 0; i < this.numItem; i++) {
+            for (var j = 0; j < this.numItem; j++) {
+                var node = cc.instantiate(this.pre_item_bg);
                 node.parent = this.itemParent;
                 node.width = this.itemW;
                 node.height = this.itemW;
-                node.anchorX = 0;
-                node.anchorY = 0;
-                node.x = posStart.x+(node.width + 5) * i;
-                node.y = posStart.y+(node.height + 5) * j;
+                node.x = posStart.x + (node.width + 5) * i;
+                node.y = posStart.y + (node.height + 5) * j;
             }
         }
     },
 
-    init: function ()
-    {
-       
+    addItem: function () {
+        var posStart = cc.v2(-this.itemParentW / 2 + this.itemW / 2 + this.interval, -this.itemParentW / 2 + this.itemW / 2 + this.interval);
+        for (var i = 0; i < this.numItem; i++) {
+            for (var j = 0; j < this.numItem; j++) {
+                if (this.array[i][j] != 0) {
+                    var node = cc.instantiate(this.pre_item);
+                    node.parent = this.itemParent;
+                    node.width = this.itemW;
+                    node.height = this.itemW;
+                    node.x = posStart.x + (node.width + 5) * i;
+                    node.y = posStart.y + (node.height + 5) * (this.numItem-1-j);
+                }
+            }
+        }
+    },
+
+    init: function () {
+
         this.interval = 5;//间隔
         this.itemW = Math.round(650 / this.numItem);//块大小
         this.itemParentW = this.numItem * this.itemW + (this.numItem + 1) * this.interval;//背景大小    
         this.itemParent.width = this.itemParentW;
         this.itemParent.height = this.itemParentW;
+
+        this.initArray();
+        this.addItemBg();
         this.addItem();
     },
 
-    cleanItem: function ()
-    {
+    initArray: function () {
+        this.array = new Array();
+        for (let i = 0; i < this.numItem; i++) {
+            this.array[i] = new Array();
+        }
+
+        for (let i = 0; i < this.numItem; i++) {
+            for (let j = 0; j < this.numItem; j++) {
+                this.array[i][j] = 0;
+            }
+        }
+
+        this.array[0][0] = 2;
+        this.array[1][1] = 2;
+        cc.log(this.array);
+    },
+
+    cleanItem: function () {
         this.itemParent.removeAllChildren();
     }
     // update (dt) {},
